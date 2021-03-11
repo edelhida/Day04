d732f221d8e0da5344a291e2608c68a4b916d8dd
edelhida <edelhida.dervisevic@gmail.com>
2021-03-11 18:13:53 +0100

    Task05 changed

--- ex05/src/index.js
+++ ex05/src/index.js
@@ -1,14 +1,14 @@
 function main(numOne, numTwo) {
     // Only change code below this line 
-    var product = parseFloat(numOne * numTwo);
+    var product = numOne * numTwo;
     
         // Only change code above this line 
     return product;
     
     }
     
-    console.log(main(parseFloat(2.5), parseFloat(2.0)).toFixed(1));
-    console.log(main(parseFloat(5.5), parseFloat(5.5)).toFixed(2));
-    console.log(main(parseFloat(100.25), parseFloat(43.20)).toFixed(1));
+    console.log(main(2.5, 2.0));
+    console.log(main(5.5, 5.5));
+    console.log(main(100.25, 43.20));
 
     module.exports = main;
